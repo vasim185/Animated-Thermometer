c503a2e157e967d388c76850adebf4359bb57ba2
vasim185 <69073141+vasim185@users.noreply.github.com>
2021-03-17 15:01:15 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+const tempUp = () => {
+  let temp = document.getElementById("temp");
+  temp.innerHTML = "&#xf2cb";
+
+
+  setTimeout(() => {
+    temp.innerHTML = "&#xf2ca";
+    temp.style.color = "powderblue";
+  }, 1000);
+
+  setTimeout(() => {
+    temp.innerHTML = "&#xf2c9";
+    temp.style.color = "green";
+  }, 2000);
+
+  setTimeout(() => {
+    temp.innerHTML = "&#xf2c8";
+    temp.style.color = "orange";
+  }, 3000);
+
+  setTimeout(() => {
+    temp.innerHTML = "&#xf2c7";
+    temp.style.color = "red";
+  }, 4000);
+}
+
+tempUp();
